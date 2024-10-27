82e941bd92da1a298ecdab125e4969a6b5c46ec0
Abdul Rahmna Al Saadi <abedsaadi987@gmail.com>
2024-10-27 13:56:39 +0200

    Created map layout

--- game.js
+++ game.js
@@ -1,7 +1,38 @@
+class MyScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'MyScene' });
+    }
+    
+    preload() {
+        this.load.image('background', 'assets/images/background-img.jpg');
+        this.load.image('tiles', 'assets/tileset/dirtWall.png');
+        this.load.tilemapTiledJSON('map', 'assets/tileset/map.json');
+        this.load.spritesheet('coin', 'assets/images/coin.png', { frameWidth: 15, frameHeight: 16 });
+    }
+
+    
+    create() {
+    
+        let background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        background.setDisplaySize(960, 640);
+
+        const map = this.make.tilemap({ key: 'map' });
+        const tiles = map.addTilesetImage('mazeWallsTileset', 'tiles');
+        const layer = map.createLayer("Tile Layer 1", tiles, 0, 0);
+        background.setDepth(0);
+        layer.setDepth(1);
+    }
+    
+    update() {
+        
+    }
+    
+}
+
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 960,
+    height: 640,
     physics: {
         default: "arcade",
         arcade: {
@@ -9,87 +40,8 @@ var config = {
             debug: false,
         },
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update,
-    },
+    scene: MyScene
 };
 
 var game = new Phaser.Game(config);
 
-// class PreloadScene extends Phaser.Scene {
-//     constructor() {
-//         super({ key: 'PreloadScene' });
-//     }
-
-//     preload() {
-//         // Load the tilemap
-//         this.load.tilemapTiledJSON('map', 'assets/tilemaps/maze-map.json');
-
-//         // Load the tileset image
-//         this.load.tilemapTiledJSON("map", "maze-map.json");
-//     }
-
-//     create() {
-//         // After loading, switch to the GameScene
-//         this.scene.start('GameScene');
-//     }
-// }
-// class GameScene extends Phaser.Scene {
-//     constructor() {
-//         super({ key: 'GameScene' });
-//     }
-
-//     create() {
-//         // Get the tilemap
-//         const map = this.make.tilemap({ key: 'map' });
-
-//         // Add the tileset to the map
-//         const tileset = map.addTilesetImage('tileset-img', './assets/tilemaps/tileset-img'); // Replace 'tilesetName' with the name you used in Tiled
-
-//         // Create layers (replace 'layerName' with the actual name of your layer in Tiled)
-//         const layer = map.createLayer('layerName', tileset, 0, 0);
-        
-//         // If you have multiple layers, you can create them like this:
-//         // const backgroundLayer = map.createLayer('BackgroundLayer', tileset, 0, 0);
-//         // const foregroundLayer = map.createLayer('ForegroundLayer', tileset, 0, 0);
-//     }
-// }
-
-
-
-function preload() {
-    this.load.image("background-img", "assets/images/background-img.jpg");
-    this.load.image('ground', 'assets/images/platform.png');
-    // this.load.tilemapTiledJSON("map", "assets/tilemaps/maze.tmj");
-    // this.load.image("tiles", "assets/tilemaps/tileset-img.png");
-    // this.load.tilemapTiledJSON("map", "maze-map.json");
-    // this.load.spritesheet("dude", "assets/dude.png", {
-    //     frameWidth: 32,
-    //     frameHeight: 48,
-    // });
-}
-
-function create() {
-    this.add.image(400, 300, "background-img");
-    // const map = this.make.tilemap({ key:'map'});
-    // const tiles = map.addTilesetImage('tileset-img', 'tiles');
-    // const layer = map.createLayer(0, tiles, 0, 0);
-    // const map = this.add.tilemap("map");
-    // const tiles = map.addTilesetImage("maze", "tiles");
-    // const groundLayer = map.createLayer("Tile Layer 1", tiles);
-    platforms = this.physics.add.staticGroup();
-
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-
-
-}
-
-function update() {
-
-}
